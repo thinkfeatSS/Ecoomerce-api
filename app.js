81a182aa5716dcc2e4f0b3ce2a040448c380fd67
unknown <thinkfeat@gamil.com>
2023-12-20 18:44:33 +0500

    authentication is continue

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.get("/", (req, res) =>{
     res.send("Hello from api")
 });
 
-app.use("/api/v1",userRoutes);
-// Roting
+// app.use("/api/v1",userRoutes);
+// // Roting
 
 
 module.exports = app
