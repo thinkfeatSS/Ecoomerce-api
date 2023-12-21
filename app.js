66515ce1e784bda7316f6a27250537c56d9d6f33
unknown <thinkfeat@gamil.com>
2023-12-21 11:32:49 +0500

    auth

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-const userRoutes = require("./routes/userRoutes");
 const app = express();
 app.use(express.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({
+      path: "config/.env",
+    });
+  }
+
 app.get("/", (req, res) =>{
     res.send("Hello from api")
 });
+
+const userRoutes = require("./routes/userRoutes");
 
 // app.use("/api/v1",userRoutes);
 // // Roting
